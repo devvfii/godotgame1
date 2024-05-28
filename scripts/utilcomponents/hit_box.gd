c03a35eca9feefdb56473c2be2b7bac1ac787205
devvfii <four321gaming@gmail.com>
2024-05-28 16:15:50 +0800

    fixed mess with mixing up hurtboxes and hitboxes

--- scripts/utilcomponents/hit_box.gd
+++ scripts/utilcomponents/hit_box.gd
@@ -1,7 +1,7 @@
 extends Area2D
 class_name HitBox
 
-@export var health_component : HealthComponent
+@export var damage : int
 
 @onready var collision = $CollisionShape2D
 @onready var timer = $Timer
@@ -10,9 +10,6 @@ func tempdisable():
 	collision.call_deferred("set","disabled", true)
 	timer.start()
 	
-func damage(attack: Attack):
-	if health_component:
-		health_component.damage(attack)
 
 func _on_timer_timeout():
 	collision.call_deferred("set","disabled", false)
